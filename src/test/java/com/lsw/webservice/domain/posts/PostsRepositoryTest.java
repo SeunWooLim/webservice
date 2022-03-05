69717ad9bf6ac00b77bce68a08aaa6d763a2643d
MSI\dlatm <dlatmddn468@gmail.com>
2022-03-05 22:28:32 +0900

    add PostsRepositoryTest

--- src/test/java/com/lsw/webservice/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/lsw/webservice/domain/posts/PostsRepositoryTest.java
@@ -6,13 +6,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@WebAppConfiguration
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
