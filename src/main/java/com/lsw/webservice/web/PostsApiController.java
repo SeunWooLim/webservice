b601ed31ce45295cbd1695eeee02eda0edb0904c
MSI\dlatm <dlatmddn468@gmail.com>
2022-03-05 23:44:03 +0900

    posts update 추가

--- src/main/java/com/lsw/webservice/web/PostsApiController.java
+++ src/main/java/com/lsw/webservice/web/PostsApiController.java
@@ -1,10 +1,11 @@
 package com.lsw.webservice.web;
 
 import com.lsw.webservice.service.posts.PostsService;
+import com.lsw.webservice.web.dto.PostsResponseDto;
 import com.lsw.webservice.web.dto.PostsSaveRequestDto;
+import com.lsw.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,8 +13,18 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 }
