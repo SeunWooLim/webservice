701c3c10b02ca1372e6b29ba076a26cca082e690
MSI\dlatm <dlatmddn468@gmail.com>
2022-03-05 21:12:34 +0900

    change return type String to Dto

--- src/main/java/com/lsw/webservice/web/HelloController.java
+++ src/main/java/com/lsw/webservice/web/HelloController.java
@@ -1,13 +1,17 @@
 package com.lsw.webservice.web;
 
+import com.lsw.webservice.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    public HelloResponseDto hello(@RequestParam("name") String name,
+                        @RequestParam("amount") int amount
+                        ){
+        return new HelloResponseDto(name, amount);
     }
 }
