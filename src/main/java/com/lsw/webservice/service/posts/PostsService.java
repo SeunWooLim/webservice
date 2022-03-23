618065d9b948eef9b20bc9d6a56ed53d719184d6
MSI\dlatm <dlatmddn468@gmail.com>
2022-03-24 00:09:25 +0900

    add delete action

--- src/main/java/com/lsw/webservice/service/posts/PostsService.java
+++ src/main/java/com/lsw/webservice/service/posts/PostsService.java
@@ -54,7 +54,7 @@ public class PostsService {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
-        postsRepository.delete(posts);
+        postsRepository.delete(posts); 
     }
 
 }
