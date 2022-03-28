ae27ac7cea3ea5163132f030e31f4db4b188a574
MSI\dlatm <dlatmddn468@gmail.com>
2022-03-28 21:17:07 +0900

    Add User entity, repository, enum

---
+++ src/main/java/com/lsw/webservice/domain/user/User.java
@@ -0,0 +1,45 @@
+package com.lsw.webservice.domain.user;
+
+import com.lsw.webservice.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role){
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public String getRoleKey(){
+        return this.role.getKey();
+    }
+
+}
