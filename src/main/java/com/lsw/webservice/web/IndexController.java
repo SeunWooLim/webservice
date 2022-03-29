3cdcb8b1559acbd424e74f1415ebc2970a0bcd87
MSI\dlatm <dlatmddn468@gmail.com>
2022-03-29 23:59:22 +0900

    세션 유저정보 어노테이션 설정

--- src/main/java/com/lsw/webservice/web/IndexController.java
+++ src/main/java/com/lsw/webservice/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.lsw.webservice.web;
 
+import com.lsw.webservice.config.auth.LoginUser;
 import com.lsw.webservice.config.auth.dto.SessionUser;
 import com.lsw.webservice.service.posts.PostsService;
 import com.lsw.webservice.web.dto.PostsResponseDto;
@@ -19,9 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
